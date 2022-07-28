b657f9a5833cc0555bd8eb010d7335d0e016a072
lahiruc@gl.block-stars.com <lahiruc@gl.block-stars.com>
2022-07-28 12:20:00 +0530

    mongo db

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const { connectToDb, getDb } = require("./db");
+
+//init app & middleware
+const app = express();
+app.use(express.json());
+
+//db connection
+let db;
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log("App listening on port 3000");
+    });
+    db = getDb();
+  }
+});
+
+//routes
+app.get("/books", (req, res) => {
+  //current page
+  const page = req.query.p || 0;
+  const booksPerPage = 3;
+
+  let books = [];
+
+  db.collection("books")
+    .find()
+    .sort({ author: 1 })
+    .skip(page * booksPerPage)
+    .limit(booksPerPage)
+    .forEach((book) => books.push(book))
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Could not fetch the document" });
+    });
+});
+
+app.get("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .findOne({ _id: ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not fetch the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
+
+app.post("/books", (req, res) => {
+  const book = req.body;
+
+  db.collection("books")
+    .insertOne(book)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Could not create new document" });
+    });
+});
+
+app.delete("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((error) => {
+        res.status(500).json({ error: "Could not delete the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
+
+app.patch("/books/:id", (req, res) => {
+  const updates = req.body;
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .updateOne({ _id: ObjectId(req.params.id) }, { $set: updates })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((error) => {
+        res.status(500).json({ error: "Could not update the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
